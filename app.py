d10e7e58ed5e54f89f24089ac2688ffd046a056a
pbernhardt <papadiscobravo@users.noreply.github.com>
2021-03-26 09:23:55 -0500

    app.py starting to come together now

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 # This is app.py for my HW10 assignment: SQLAlchemy Challenge.
-
 # starter files:
 # hawaii.sqlite
 # SQLAlchemyChallenge.ipynb
 
-# import numpy, sqlalchemy, and flask
-import numpy as np
 
+# 1 Import Flask...
+from flask import Flask, jsonify
+
+# ... et al.
+import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+
+# 2 Set up Flask / create an app
+app = Flask(__name__)   
+
+
+# 2.1 do database setup
 
 # set up database
 engine = create_engine("sqlite:///hawaii.sqlite")
@@ -23,135 +30,121 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# save reference to the table
-Weather = Base.classes.measurement
+# hawaii.sqlite has two tables:
+# Station
+# Measurement
+# I worked with them in Jupyter Notebook.
+# Save references to each table
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
-# create an app
-app = Flask(__name__)   
+
+# 3. Define routes
 
 # list routes
 # /
 #   Home page.
+#   List all routes that are available.
+#   the four routes:
+#      precip
+#      stations
+#      tobs
+#      start and end
+
 @app.route("/")
 def home():
-    print("Server received request for HW10-Hawaii weather home page...")
-    return "Welcome to the HW10-Hawaii weather home page."
+#    print(f"Server received request for HW10-Hawaii weather home page...")
+    return "Welcome to the HW10-Hawaii weather home page. Your routes are:<br>\
+    <a href='/api/v1.0/precip'>/api/v1.0/precip</a><br>\
+    <a href='/api/v1.0/stations'>/api/v1.0/stations</a><br>\
+    <a href='/api/v1.0/tobs'>/api/v1.0/tobs</a><br>\
+    <a href='/api/v1.0/<start_date>'>/api/v1.0/<start_date></a><br>\
+    <a href='/api/v1.0/contact'>/api/v1.0/contact</a><br>"
 
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page."
 
-#
-#
-#
-#   List all routes that are available.
+# /api/v1.0/precip
 
-# /api/v1.0/precipitation
-weather_dict = {
-    "date": [],
-    "prcp": []
-    }
+# a Convert the query results to a dictionary using date as the key and prcp as the value.
+# b Return the JSON representation of your ditionary.
 
-@app.route("/normal")
-def normal():
-    return weather_dict
-
-@app.route("/jsonified")
-def jsonified():
-    return jsonify(weather_dict)
-
-#   Convert the query results to a dictionary using date as the key and prcp as the value.
-#   Return the JSON representation of your dictionary.
-
-@app.route("/api/v1.0/weather")
-def weather():
-    # Create our session (link) from Python to the DB
+@app.route("/api/v1.0/precip")
+def precip():
+#    print(f"Server received request for station data...")
+# Create session (link) from Python to the database: 
     session = Session(engine)
 
-    weather_results = session.query(weather_dict.name).all()
+# Query the database:
+    results = session.query(Measurement.date, Measurement.prcp).all()
 
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
-
+# Don't forget to close the session or there will be an orphaned session
+# lurking around the Flask server trying up system resources and if that
+# happens enough times it will affect server performance and perhaps even
+# make the server behave more unhappily than that:
     session.close()
 
+# Can try returning {results} jsonified...
+#    return jsonify(results)
 
-
+# ...or, if {results} is a list of tuples, convert it into a normal list:
+    all_measurements = list(np.ravel(results))
+    return jsonify(all_measurements)
 
 
 # /api/v1.0/stations
-#   Return a JSON list of stations from the dataset.
+
+# weather_stations = {"name": [], "id": [], "elevation": [], "latitude": [], "station": [], "longitude": []}
+
+# Return a JSON list of stations from the dataset.
+# (There must be a query to populate weather_stations.)
+
+@app.route("/api/v1.0/stations")
+def stations():
+#    print(f"Server received request for station data...")
+    session = Session(engine)
+    results = session.query(Station.name).all()
+    session.close()
+    weather_stations = list(np.ravel(results))
+#    return f"Here are the Hawaii weather station in the data.<br>\
+#    <a href='/'>home</a>br\
+#    {jsonify(weather_stations)}"
+    return jsonify(weather_stations)
 
 # /api/v1.0/tobs
-#   Query the dates and temperature observations of the most active station for the last year of data.
-#   Return a JSON list of temperature observations (TOBS) for the previous year.
 
-# /api/v1.0/<start> and /api/v1.0/<start>/<end>
-#   Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-#   When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
-#   When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+tobs = {"date": [], "station": [], "tobs": []}
 
-# Hints
-#   You will need to join the station and measurement tables for some of the queries.
-#   Use Flask jsonify to convert your API data into a valid JSON response object.
+# a Query the dates and temperature observations of the most active station for the last year of data.
+# b Return a JSON list of temperature observations (TOBS) for the previous year.
+@app.route("/api/v1.0/tobs")
+def tobs():
+ #   print(f"Server received request for tobs...")
+    return "fHere's some Hawaii tobs data.<br>\
+    <a href='/'>home</a><br>\
+    {jsonify(tobs)}"
+    return jsonify(tobs)
 
+# /api/v1.0/<start_date>
 
+# start_date = "2011-01-01"
+
+@app.route("/api/v1.0/<start_date>")
+def start():
+#    print(f"Server received request for weather data starting {start_date}...")
+    return "Here's some Hawaii weather data starting {start_date}.<br>\
+    <a href='/'>home</a>"
+
+# /api/v1.0/contact
+@app.route("/api/v1.0/contact")
+def contact():
+    email = "papadiscobravo@gmail.com"
+#    print(f"Server received request for contact info...")
+    return f"Email {email} with questions.<br><a href='/'>home</a>"
+
+# 4 Define main behavior.
 #
+# LOOK      LOOK      LOOK      LOOK      LOOK      LOOK      LOOK     
+#      LOOK      LOOK      LOOK      LOOK      LOOK      LOOK      LOOK
+# Code has to have this at the end or something/everything will blow up:
 #
-#
-#
-#
-#
-#
-# more code that might be helpful?
-@app.route("/api/v1.0/justice-league")
-def justice_league():
-    """Return the justice league data as json"""
-
-    return jsonify(justice_league_members)
-
-
-@app.route("/api/v1.0/justice-league/<real_name>")
-def justice_league_character(real_name):
-    """Fetch the Justice League character whose real_name matches
-       the path variable supplied by the user, or a 404 if not."""
-
-    canonicalized = real_name.replace(" ", "").lower()
-    for character in justice_league_members:
-        search_term = character["real_name"].replace(" ", "").lower()
-
-        if search_term == canonicalized:
-            return jsonify(character)
-
-    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
-
-
-
-
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
-
-
 if __name__ == '__main__':
     app.run(debug=True)
